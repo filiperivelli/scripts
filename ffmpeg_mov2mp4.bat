674d376c3a0167c41002ef798971cfd7e34a40d2
Filipe Rivelli <filiperivelli@gmail.com>
2022-04-27 17:04:12 -0300

    Adicionado a opcao de executar com aceleração de hardware

--- ffmpeg_mov2mp4.bat
+++ ffmpeg_mov2mp4.bat
@@ -5,4 +5,7 @@
 ::Cria o diretorio mp4 caso nao exista
 for %%i in (%*) do if not exist  %%~pimp4\ (mkdir %%~pimp4\)
 ::Executa o ffmpeg para criar novos arquivos mp4 dentro da pasta mp4
-for %%i in (%*) do ffmpeg -i %%i -vcodec h264 -acodec aac -strict -2 .\mp4\%~n1.mp4
+for %%i in (%*) do ffmpeg -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -i %%i -vcodec h264_nvenc -acodec aac -strict -2 .\mp4\%%~ni.mp4
+
+::Executar sem aceleração de hardware
+::for %%i in (%*) do ffmpeg -i %%i -vcodec h264 -acodec aac -strict -2 .\mp4\%%~ni.mp4
