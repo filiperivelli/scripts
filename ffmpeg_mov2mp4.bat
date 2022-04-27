cfe32b86128f7cd4af6d31bef3b04908a4124f07
Filipe Rivelli <filiperivelli@gmail.com>
2022-04-27 16:47:04 -0300

    Inicio script para transformar mov em mp4

---
+++ ffmpeg_mov2mp4.bat
@@ -0,0 +1,8 @@
+::Script para transformar videos mov em mp4 por Filipe Rivelli
+::Criado em 27/07/2022
+::Os arquivos de vídeo devem ser arrastados para o ícone desse script
+@echo off
+::Cria o diretorio mp4 caso nao exista
+for %%i in (%*) do if not exist  %%~pimp4\ (mkdir %%~pimp4\)
+::Executa o ffmpeg para criar novos arquivos mp4 dentro da pasta mp4
+for %%i in (%*) do ffmpeg -i %%i -vcodec h264 -acodec aac -strict -2 .\mp4\%~n1.mp4
