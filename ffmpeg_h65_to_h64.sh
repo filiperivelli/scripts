86d8bff4a1714655df8e6174a7a375a8b1d0be7c
Filipe Rivelli <40794249+filiperivelli@users.noreply.github.com>
2022-04-28 11:13:05 -0300

    Update ffmpeg_h65_to_h64.sh

--- ffmpeg_h65_to_h64.sh
+++ ffmpeg_h65_to_h64.sh
@@ -2,6 +2,7 @@
 Sript para fazer o trasncode de h65 para h64
 Data: 28/04/2022
 Por: Filipe Rivelli
+https://superuser.com/questions/1380946/how-do-i-convert-10-bit-h-265-hevc-videos-to-h-264-without-quality-loss
 '
 for var in "$@"
 do
