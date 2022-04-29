20da6c46d666af800038f197704e6b0d3cff58d8
Filipe <filiperivelli@gmail.com>
2022-04-29 15:57:02 -0300

    Alteracoes

--- ffmpeg_h65_to_h64.sh
+++ ffmpeg_h65_to_h64.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 '
 Sript para fazer o trasncode de h65 para h64
 Data: 28/04/2022
@@ -6,5 +7,5 @@ https://superuser.com/questions/1380946/how-do-i-convert-10-bit-h-265-hevc-video
 '
 for var in "$@"
 do
-    mv "$var" "$var".old
+    ffmpeg -i "$var" -map 0 -c:v libx265 -crf 20 -vf format=yuv420p -c:a copy "$var".mkv
 done
