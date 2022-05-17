6aa538ef4cce22635611591a3fcb31e9db0e72a1
Filipe Rivelli <filiperivelli@gmail.com>
2022-05-17 18:57:20 -0300

    Script para aumentar volume

---
+++ ffmpeg_increase_volume.bat
@@ -0,0 +1,10 @@
+::Script to increase volume sound por Filipe Rivelli
+::Criado em 17/05/2022
+::Usando o ffmpeg version 2022-04-25
+::Os arquivos de vídeo devem ser arrastados para o ícone desse script
+@echo off
+::Cria o diretorio Volume
+for %%i in (%*) do if not exist  %%~piVolume\ (mkdir %%~piVolume\)
+::Executa o ffmpeg para criar novos arquivos com volume mais alto dentro da pasta Volume
+for %%i in (%*) do ffmpeg -i %%i -filter:a "volume=1.5"  -vcodec copy %%~piVolume\%%~ni%%~xi
+pause
