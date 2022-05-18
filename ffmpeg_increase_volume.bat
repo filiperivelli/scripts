a7f45b0231b2eda02304e122f4df5a4eb81c564e
Filipe Rivelli <40794249+filiperivelli@users.noreply.github.com>
2022-05-18 13:07:06 -0300

    Simplificação de código.

--- ffmpeg_increase_volume.bat
+++ ffmpeg_increase_volume.bat
@@ -4,7 +4,7 @@
 ::Os arquivos de vídeo devem ser arrastados para o ícone desse script
 @echo off
 ::Cria o diretorio Volume
-for %%i in (%*) do if not exist  %%~piVolume\ (mkdir %%~piVolume\)
+for %%i in (%*) do if not exist  .\Volume\ (mkdir .\Volume\)
 ::Executa o ffmpeg para criar novos arquivos com volume mais alto dentro da pasta Volume
 for %%i in (%*) do ffmpeg -i %%i -filter:a "volume=1.5"  -vcodec copy ".\Volume\%%~ni%%~xi"
 pause
